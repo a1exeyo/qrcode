277ce7d87137abdc68b98ff34e3f8919dfa082ee
Alexey <goodman39608@gmail.com>
2025-05-03 01:09:25 +0300

    deploy 6

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './layout';
-import { Navigation } from './Components/Navigation/Navigation';
+import { Navigation } from './Components/Navigation/navigation';
 
 
 
